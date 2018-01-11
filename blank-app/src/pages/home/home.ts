723a18f6c65cf9e5466b67c521e57a2d3a71beb9
thu <mingfei.net@gmail.com>
2018-01-11 11:48:37 +0800

    Update

--- blank-app/src/pages/home/home.ts
+++ blank-app/src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {ButtonPage} from "../button/button";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  buttonPage;
 
+  constructor(public navCtrl: NavController) {
+    this.buttonPage = ButtonPage;
   }
 
 }
