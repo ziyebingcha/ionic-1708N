1fbb14ab6e1476652348a3cf8485ff3b03bd5050
thu <mingfei.net@gmail.com>
2018-01-11 16:57:53 +0800

    Update

--- blank-app/src/pages/list/list.ts
+++ blank-app/src/pages/list/list.ts
@@ -15,21 +15,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
 
-  items = [
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry',
-    'Tom',
-    'Jerry'
+  items = [ // todo 数据来自于 HTTP 请求
+    'Tom 1',
+    'Jerry 1',
+    'Tom 2',
+    'Jerry 2',
+    'Tom 3',
+    'Jerry 3',
+    'Tom 4',
+    'Jerry 4',
+    'Tom 5',
+    'Jerry 5',
+    'Tom 6',
+    'Jerry 6',
+    'Tom 7',
+    'Jerry 7'
   ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -37,6 +37,10 @@ export class ListPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListPage');
+  }
+
+  itemSelected(item: string){
+    console.error(`selected: ${item}`);
   }
 
 }
