3b87835a7da460f62c35e9a3202212ded8b4273b
thu <mingfei.net@gmail.com>
2018-01-12 17:58:27 +0800

    Update

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 
 let app = new express();
 
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
    console.log('server listen to 3000...');
 });
 
-app.listen(300);
+// 处理用户的注册请求 POST
+app.get('/signUp', (req, res) => {
+    console.log(`用户名：${req.query.username}`);
+    console.log(`密码：${req.query.password}`);
+});
+
+
+app.listen(3000);
