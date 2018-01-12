f77a9968ec4136f085b3756a261077e24728311c
thu <mingfei.net@gmail.com>
2018-01-12 17:02:06 +0800

    Update

--- blank-app/src/pages/icon/icon.ts
+++ blank-app/src/pages/icon/icon.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IconPage {
 
+  testIcon:string = 'medical';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
