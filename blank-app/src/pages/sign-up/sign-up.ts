b6df51e339eec80a451e41e432aa328a019697db
LiWenhua <lwh@tedu.cn>
2018-01-15 15:42:37 +0800

    Update

--- blank-app/src/pages/sign-up/sign-up.ts
+++ blank-app/src/pages/sign-up/sign-up.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Http, Response} from "@angular/http";
 
 /**
  * Generated class for the SignUpPage page.
@@ -20,20 +21,23 @@ export class SignUpPage {
     password: ''
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // 依赖注入（DI） Http
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignUpPage');
   }
 
-  signUp():void {
+  signUp(): void {
     let username = this.User.username;
     let password = this.User.password;
     console.error(`username: ${username}`);
     console.error(`password: ${password}`);
 
-    // HTTP request to Server
+// todo HTTP request to Server
 
   }
 
--- blank-server/app.js
+++ blank-server/app.js
@@ -3,7 +3,18 @@ const express = require('express');
 let app = new express();
 
 app.get('/', (req, res) => {
-    res.end("server is running on port 3000...");
+    res.end('server is running on port 3000...');
 });
+
+// http://127.0.0.1:3000/signUp?username=Tom&password=123
+app.get('/signUp', (req, res) => {
+    console.log(req.query.username);
+    console.log(req.query.password);
+
+    // todo save user into DB: INSERT INTO...
+
+    res.end('sign up...');
+});
+
 
 app.listen(3000);
