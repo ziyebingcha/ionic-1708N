c1a4898f667c308360ea784fb4588467a8d28a90
LiWenhua <lwh@tedu.cn>
2018-01-16 11:07:01 +0800

    Update

--- blank-app/src/app/app.module.ts
+++ blank-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import {Gesture, IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import {HttpModule} from "@angular/http";
